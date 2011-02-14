0bd790e1665767447e242523e202b111713ade21
pellomugica <pellomugica@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-02-14 09:09:15 +0000

    Creacion .class ventana

---
+++ trunk/src/Ventanaprincipal.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+
+public class Ventanaprincipal extends JFrame implements ActionListener {
+
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
