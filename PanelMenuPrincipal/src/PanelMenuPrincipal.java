fea45f85b26291bfb1f1e74567186582d943e5cf
pellomugica <pellomugica@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-02-14 15:34:02 +0000

    Action listener de salir

--- PanelMenuPrincipal/src/PanelMenuPrincipal.java
+++ PanelMenuPrincipal/src/PanelMenuPrincipal.java
@@ -5,47 +5,56 @@ import javax.swing.*;
 
 
 public class PanelMenuPrincipal extends JPanel implements ActionListener{
-	JPanel J;
-	JButton pRapida;//Partida Rapida
-	JButton pPersonalizada;//Partida Personalizada
-	JButton pTutorial;//Tutorial
-	//JButton bOpciones;//Entrar opciones
-	JButton bSalir;//Boton Salir
+	JPanel J; 
+	JFrame f; //Ventana del juego
+	JButton pRapida; //Partida Rapida
+	JButton pPersonalizada; //Partida Personalizada
+	JButton pTutorial; //Tutorial
+	JButton pOnline; // Online
+	JButton bEstadistica; //Ver estadisticas
+	JButton bSalir; //Boton Salir
 
-	public PanelMenuPrincipal(){
+	public PanelMenuPrincipal(JFrame z){
 		//Formato Ventana
+		f=z;
 		J=new JPanel();
 		J.setLayout(new SpringLayout());
 		
 		//Declaracion JButtons
 		pRapida = new JButton("Partida rapida");
 		pPersonalizada = new JButton("Partida Personalizada");
+		pOnline = new JButton("Partida online");
 		pTutorial = new JButton ("Partida Tutorial");
+		bEstadistica = new JButton ("Estadistica");
 		bSalir = new JButton ("Salir");
 		
 		//Poner ActionListener Botones
 		pRapida.addActionListener(this);
 		pPersonalizada.addActionListener(this);
 		pTutorial.addActionListener(this);
+		pOnline.addActionListener(this);
+		bEstadistica.addActionListener(this);
 		bSalir.addActionListener(this);
 		
 		//Poner Botones
 		J.add(pRapida);
 		J.add(pPersonalizada);
+		J.add(pOnline);
 		J.add(pTutorial);
+		J.add(bEstadistica);
 		J.add(bSalir);
-		SpringUtilities.makeCompactGrid(J,4,1,6,6,6,6);
+		SpringUtilities.makeCompactGrid(J,6,1,6,6,6,6);
 		this.add(J);
-		
-		
 	}
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getSource()==bSalir){
+			f.dispose();
+		}
 	}
 
 	public static void main(String[]args){
 		JFrame v = new JFrame();
-		PanelMenuPrincipal p=new PanelMenuPrincipal();
+		PanelMenuPrincipal p=new PanelMenuPrincipal(v);
 		v.add(p);
 		v.setVisible(true);
 	}
