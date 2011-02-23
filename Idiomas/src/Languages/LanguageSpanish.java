3a5d736cda46e1345d15e402b75dcbc8a3aa914f
1joroji@gmail.com <1joroji@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-02-23 15:45:58 +0000

    Idiomas añadidos

---
+++ Idiomas/src/Languages/LanguageSpanish.java
@@ -0,0 +1,19 @@
+package Languages;
+
+public class LanguageSpanish extends Language {
+	
+	public LanguageSpanish(){
+		this.words.put("aceptar", "Aceptar");
+		this.words.put("salir", "Salir");
+		this.words.put("nuevapartida", "Nueva partida");
+		this.words.put("opciones", "Opciones");
+		this.words.put("partidarapida", "Partida rápida");
+		this.words.put("partidapersonalizada", "Partida personalizada");
+		this.words.put("nuevojugador", "Nuevo jugador");
+		this.words.put("tutorial", "Tutorial");
+		this.words.put("unidades", "Unidades");
+		this.words.put("oro", "Oro");
+
+	}
+	
+}
