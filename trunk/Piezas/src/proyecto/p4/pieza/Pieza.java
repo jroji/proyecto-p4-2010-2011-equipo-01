29d0c07f7d7707ca5486e90451b3ab4382b41ffc
julentv@gmail.com <julentv@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-02-23 14:41:58 +0000

    Clase pieza e interfaz Moveable

---
+++ trunk/Piezas/src/proyecto/p4/pieza/Pieza.java
@@ -0,0 +1,27 @@
+package proyecto.p4.pieza;
+
+import javax.swing.ImageIcon;
+
+/* Clase padre de las piezas de todos los juegos
+ * @implements Moveable 
+ * 
+ */
+public abstract class Pieza implements Moveable{
+	enum Colores {blanco, negro}
+	private int position_x;
+	private int position_y;
+	//color que indica el bando al que pertenece la pieza
+	private Colores color;
+	private ImageIcon imagen;
+	//array bidimensional booleano que indica las casillas a las que se puede mover
+	private boolean [][] able_to_move;
+	
+	public void setAbleToMove (){
+		for (int i =0; i<able_to_move.length;i++)
+		{
+			for(int j=0; j<able_to_move[i].length;j++)
+				able_to_move [i][j]= canMove (i,j);
+		}
+	}
+
+}
