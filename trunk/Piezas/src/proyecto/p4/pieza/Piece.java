bb4690dce60d15900e6b50da184ba9fbc1bf3b79
julentv@gmail.com <julentv@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-02-24 20:06:55 +0000

    comentarios de alguno metodos

--- trunk/Piezas/src/proyecto/p4/pieza/Piece.java
+++ trunk/Piezas/src/proyecto/p4/pieza/Piece.java
@@ -5,6 +5,7 @@ import javax.swing.ImageIcon;
 /* Clase padre de las piezas de todos los juegos.
  * Es abstracta, puesto que no se quiere permitir instanciar objetos de este tipo, sino
  * de sus clases hijas.
+ * 
  * @implements Moveable 
  * 
  */
@@ -12,12 +13,20 @@ public abstract class Piece implements Moveable{
 	public enum Colores {blanco, negro}
 	private int position_x;
 	private int position_y;
+	
 	//color que indica el bando al que pertenece la pieza
 	private Colores color;
 	private ImageIcon imagen;
+	
 	//array bidimensional booleano que indica las casillas a las que se puede mover
 	private boolean [][] able_to_move;
 	
+	/* Metodo que modifica el atributo able_to_move escribiendo true en las posiciones a las que 
+	 * puede moverse la pieza y false en las que no en base al resultado devuelto por el metodo
+	 * canMove.
+	 * 
+	 * @see Moveable.#canMove
+	 */
 	public void setAbleToMove (){
 		for (int i =0; i<able_to_move.length;i++)
 		{
