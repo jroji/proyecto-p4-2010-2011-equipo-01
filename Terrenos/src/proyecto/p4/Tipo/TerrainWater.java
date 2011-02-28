2b4ce9fa8f4da85d5d43a0498b493b8bd3744025
raquel.cagigas2 <raquel.cagigas2@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-02-28 11:54:24 +0000

    Modificación del constructor

--- Terrenos/src/proyecto/p4/Tipo/TerrainWater.java
+++ Terrenos/src/proyecto/p4/Tipo/TerrainWater.java
@@ -3,9 +3,10 @@ package proyecto.p4.Tipo;
 import javax.swing.ImageIcon;
 
 public class TerrainWater extends Terrain{
-	
+	public TerrainWater(){
 	name = "Water";
 	movement = 2;
-	image = new ImageIcon("Aqui la ruta de la imagen");
-	
+	String ruta= "";
+	image = new ImageIcon(ruta);
+	}
 }
