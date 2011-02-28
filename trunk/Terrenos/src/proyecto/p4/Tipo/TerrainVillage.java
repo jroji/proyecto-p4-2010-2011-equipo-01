b08f66f13f84f1ae5298fab5c4771786b1870816
1joroji@gmail.com <1joroji@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-02-28 11:46:02 +0000

    Terreno villa

---
+++ trunk/Terrenos/src/proyecto/p4/Tipo/TerrainVillage.java
@@ -0,0 +1,11 @@
+package proyecto.p4.Tipo;
+
+import javax.swing.ImageIcon;
+
+public class TerrainVillage extends Terrain{
+	
+	name = "Village";
+	movement = 1;
+	image = new ImageIcon("Aqui la ruta de la imagen");
+	
+}
