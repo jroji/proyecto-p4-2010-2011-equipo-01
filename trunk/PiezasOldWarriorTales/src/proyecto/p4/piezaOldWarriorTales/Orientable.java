8a916ddefca292eb55dfd2f3960bee5232e0eaa1
raquel.cagigas2 <raquel.cagigas2@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-03-08 09:39:22 +0000

    j

--- trunk/PiezasOldWarriorTales/src/proyecto/p4/piezaOldWarriorTales/Orientable.java
+++ trunk/PiezasOldWarriorTales/src/proyecto/p4/piezaOldWarriorTales/Orientable.java
@@ -1,5 +1,7 @@
 package proyecto.p4.piezaOldWarriorTales;
 
+import proyecto.p4.piezaOldWarriorTales.Orientations;
+
 public interface Orientable {
 	public void setOrientacion (Orientations orientation);
 
