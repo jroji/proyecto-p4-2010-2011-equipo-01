a33c0d7204c3b370f7607352a838a935a387fdfa
raquel.cagigas2 <raquel.cagigas2@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-03-14 11:40:11 +0000

    proyecto juego

---
+++ Juego/src/proyecto/p4/Juego/Juego.java
@@ -0,0 +1,10 @@
+package proyecto.p4.Juego;
+
+import proyecto.p4.Jugador.Jugador;
+import proyecto.p4.pieza.Piece;
+
+public class Juego {
+private Jugador jugador1;
+private Jugador jugador2;
+
+}
