bad1ae2e16fa34cb72efe17dca9acea52beb0b0d
1joroji@gmail.com <1joroji@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-03-28 13:53:03 +0000

    BotoneraH

---
+++ trunk/Ventana/src/proyecto/p4/Ventana/Button/BotoneraH.java
@@ -0,0 +1,49 @@
+package proyecto.p4.Ventana.Button;
+
+/**
+ *
+ * @author Jon
+ *
+ *
+ */
+public class BotoneraH extends javax.swing.JPanel {
+
+    private OldWarriorButton oldWarriorButton1;
+    private OldWarriorButton oldWarriorButton2;
+    private OldWarriorButton oldWarriorButton3;
+	
+	private static final long serialVersionUID = 1L;
+	
+	/** Creates new form BotoneraH */
+    public BotoneraH(String Button1, String Button2,String Button3) {
+
+        oldWarriorButton1 = new OldWarriorButton(Button1);
+        oldWarriorButton2 = new OldWarriorButton(Button2);
+        oldWarriorButton3 = new OldWarriorButton(Button3);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(oldWarriorButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 134, Short.MAX_VALUE)
+                .addComponent(oldWarriorButton2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(117, 117, 117)
+                .addComponent(oldWarriorButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(oldWarriorButton3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(oldWarriorButton2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(oldWarriorButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }
+
+}
