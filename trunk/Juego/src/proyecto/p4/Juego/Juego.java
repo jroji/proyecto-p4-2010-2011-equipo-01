7ceaa1b40167d258707b54f5babba32ff4e36b6d
julentv@gmail.com <julentv@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-03-28 12:17:00 +0000

    Añadidos atributo tablero a juego

--- trunk/Juego/src/proyecto/p4/Juego/Juego.java
+++ trunk/Juego/src/proyecto/p4/Juego/Juego.java
@@ -1,10 +1,13 @@
 package proyecto.p4.Juego;
 
+import Proyecto.p4.mapa.Board;
 import proyecto.p4.Jugador.Jugador;
 import proyecto.p4.pieza.Piece;
 
 public class Juego {
-private Jugador jugador1;
-private Jugador jugador2;
+	private int identificador;
+	private Board tablero;
+	private Jugador jugador1;
+	private Jugador jugador2;
 
 }
