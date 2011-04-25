9b307b62362654aa1f88424f09de7a79877f1945
julentv@gmail.com <julentv@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-04-25 11:29:12 +0000

    añadidos metodos getPiece y getCasilla a booard

--- Mapa/src/Proyecto/p4/mapa/Board.java
+++ Mapa/src/Proyecto/p4/mapa/Board.java
@@ -1,15 +1,26 @@
 package Proyecto.p4.mapa;
 
+import proyecto.p4.pieza.Piece;
 import Proyecto.p4.casilla.Casilla;
 
 public class Board {
 	
-	private Casilla[][] Map;
+	private Casilla[][] map;
+	
+	public Board (){
+		map= new Casilla[13][13];
+	}
 	
 	public Casilla[][] getBoard(){
-		return Map;
+		return map;
 	}
 	public void setBoard(Board c){
-		Map = c.getBoard();
+		map = c.getBoard();
+	}
+	public Piece getPiece (int x, int y){
+		return map[x][y].getPiece();
+	}
+	public Casilla getCasilla(int x, int y){
+		return map[x][y];
 	}
 }
