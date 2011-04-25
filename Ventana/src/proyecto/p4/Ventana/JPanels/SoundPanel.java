1b8351da0dd008ecf30031ed98e70e71bda1e205
Pellomugica@gmail.com <Pellomugica@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-04-25 12:58:12 +0000

    Panel del sonido con codigo limpio.

---
+++ Ventana/src/proyecto/p4/Ventana/JPanels/SoundPanel.java
@@ -0,0 +1,59 @@
+ package proyecto.p4.Ventana.JPanels;
+ 
+ import javax.swing.*;
+public class SoundPanel extends javax.swing.JPanel {
+
+   
+    public SoundPanel() {
+        initComponents();
+    }
+
+   
+    private void initComponents() {
+
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        SoundLabel = new javax.swing.JLabel();
+        YesButton = new javax.swing.JRadioButton();
+        NoButton = new javax.swing.JRadioButton();
+
+        SoundLabel.setText("Sonido:");
+
+        buttonGroup1.add(YesButton);
+        YesButton.setText("Si");
+
+        buttonGroup1.add(NoButton);
+        NoButton.setText("No");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+            .addGap(25, 25, 25)
+            .addComponent(SoundLabel)
+            .addGap(18, 18, 18)
+            .addComponent(YesButton)
+            .addGap(18, 18, 18)
+            .addComponent(NoButton)
+            .addContainerGap(23, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+            .addContainerGap()
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+            .addComponent(SoundLabel)
+            .addComponent(YesButton)
+            .addComponent(NoButton))
+            .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }
+
+
+    private JRadioButton NoButton;
+    private JLabel SoundLabel;
+    private JRadioButton YesButton;
+    private ButtonGroup buttonGroup1;
+
+
+}
