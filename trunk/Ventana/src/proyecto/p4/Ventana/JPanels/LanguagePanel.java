bf6365ae3808bfe07d2488d953773ae447a08e4b
Pellomugica@gmail.com <Pellomugica@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-04-25 13:06:45 +0000

    Añadidos los metodos para saber que opciones han sido usadas.

--- trunk/Ventana/src/proyecto/p4/Ventana/JPanels/LanguagePanel.java
+++ trunk/Ventana/src/proyecto/p4/Ventana/JPanels/LanguagePanel.java
@@ -4,19 +4,22 @@ import javax.swing.*;
 
 public class LanguagePanel extends JPanel {
 
-  
+
+	private JComboBox Language;
+    private JLabel LanguageText;
+	
     public LanguagePanel() {
         initComponents();
     }
 
     private void initComponents() {
+    	
+        LanguageText = new javax.swing.JLabel();
+        Language = new javax.swing.JComboBox();
 
-        jLabel1 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
+        LanguageText.setText("Idioma:");
 
-        jLabel1.setText("Idioma:");
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Español", "Ingles", "Frances"}));
+        Language.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Español", "Ingles", "Frances"}));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -24,9 +27,9 @@ public class LanguagePanel extends JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
             .addGap(24, 24, 24)
-            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(LanguageText, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(Language, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addContainerGap(19, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -34,16 +37,16 @@ public class LanguagePanel extends JPanel {
             .addGroup(layout.createSequentialGroup()
             .addContainerGap()
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(LanguageText, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(Language, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }
 
-
+    @SuppressWarnings("unused")
+	private String GetSelection(){
+    	return (String)Language.getSelectedItem();
+    }
     
-    private JComboBox jComboBox1;
-    private JLabel jLabel1;
-
 
 }
