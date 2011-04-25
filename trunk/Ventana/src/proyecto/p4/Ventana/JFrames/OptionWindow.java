fa5ba11a6646d7d106c63037b2510b1c671fc484
Pellomugica@gmail.com <Pellomugica@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-04-25 13:18:29 +0000

    Añadida ventana de opciones.

---
+++ trunk/Ventana/src/proyecto/p4/Ventana/JFrames/OptionWindow.java
@@ -0,0 +1,89 @@
+package proyecto.p4.Ventana.JFrames;
+
+import proyecto.p4.Ventana.Button.BotoneraV;
+import proyecto.p4.Ventana.JPanels.LanguagePanel;
+import proyecto.p4.Ventana.JPanels.SoundPanel;
+
+public class OptionWindow extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = 1L;
+	public OptionWindow() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        soundPanel1 = new SoundPanel();
+        languagePanel1 = new LanguagePanel();
+        Buttons = new BotoneraV("Creditos", "Salir","","","");
+        Logo = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout ButtonsLayout = new javax.swing.GroupLayout(Buttons);
+        Buttons.setLayout(ButtonsLayout);
+        ButtonsLayout.setHorizontalGroup(
+            ButtonsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 171, Short.MAX_VALUE)
+        );
+        ButtonsLayout.setVerticalGroup(
+            ButtonsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 139, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+            .addGap(84, 84, 84)
+            .addComponent(Buttons, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+            .addGap(62, 62, 62)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(soundPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(languagePanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+            .addGroup(layout.createSequentialGroup()
+            .addGap(88, 88, 88)
+            .addComponent(Logo, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)))
+            .addGap(72, 72, 72))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addContainerGap()
+            .addComponent(Logo, javax.swing.GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(soundPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(18, 18, 18)
+            .addComponent(languagePanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(18, 18, 18)
+            .addComponent(Buttons, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(20, 20, 20))
+        );
+
+        pack();
+    }// </editor-fold>
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new OptionWindow().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify
+    private javax.swing.JPanel Buttons;
+    private javax.swing.JLabel Logo;
+    private LanguagePanel languagePanel1;
+    private SoundPanel soundPanel1;
+    // End of variables declaration
+
+}
+
