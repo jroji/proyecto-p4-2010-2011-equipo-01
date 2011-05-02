4d7cbd13effaa72dade6b27ebcb87c79c5ed895c
pellomugica@gmail.com <pellomugica@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-05-02 14:15:48 +0000

    Añadido la palabra reclutar

--- Idiomas/src/Languages/MyResources_En.java
+++ Idiomas/src/Languages/MyResources_En.java
@@ -7,7 +7,7 @@ public class MyResources_En extends ListResourceBundle{
 	public Object [][] contenido={{"label_accept","Accept"},{"label_exit","Exit"},{"label_newGame","New Game"},
 			{"label_options","Options"},{"label_quickGame","Quick Game"},{"label_customGame","Custom Game"},
 			{"label_newPlayer","New Player"},{"label_tutorial","Tutorial"},{"label_gold","Gold"},{"label_units","Units"},
-			{"label_credits","Credits"},{"label_mapEditor","Map Editor"}
+			{"label_credits","Credits"},{"label_mapEditor","Map Editor"},{"label_reclute","Reclute"}
 		
 	};
 	
