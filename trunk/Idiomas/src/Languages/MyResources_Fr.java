d82d5a2fdd95061cc23cf7379b64877482beafa3
pellomugica@gmail.com <pellomugica@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-05-05 13:48:58 +0000

    Repeticiones eliminadas

--- trunk/Idiomas/src/Languages/MyResources_Fr.java
+++ trunk/Idiomas/src/Languages/MyResources_Fr.java
@@ -10,8 +10,6 @@ public class MyResources_Fr extends ListResourceBundle{
 			{"label_newPlayer","Nouveau Joeur"},{"label_tutorial","Tutoriel"},{"label_gold","Or"},{"label_units","Unit�s"},
 			{"label_credits","Crédits"},{"label_mapEditor","Éditeur de cartes"},{"label_reclute","Recruter"},{"label_langauge","Langue"},
 			{"label_sound","Son"},{"label_attack","Attaque"},{"label_defense","Défense"},{"label_movement","Mouvement"},
-			{"label_credits","Crédits"},{"label_mapEditor","Éditeur de cartes"},{"label_reclute","Recruter"},{"label_langauge","Langue"},
-			{"label_sound","Son"},{"label_attack","Attaque"},{"label_defense","Défense"},{"label_movement","Mouvement"},
 			{"label_probability","Probabilité"},{"label_back","Retour"},{"AtacarYHuir","Attaque et de s'enfuir"},{"AtaqueCertero","Attaque précise"},
 			{"AtaqueEnZona","Zone d'attaque"},{"Cegar","Blind"},{"Convertir","Transformer"},{"CurarEstados","Etats Cure"},{"CurarZona","Zone Cure"},
 			{"Envenenar","Poison "},{"LanzarSuriquen","Paru le shuriken"},{"Sprint","Sprint"}
