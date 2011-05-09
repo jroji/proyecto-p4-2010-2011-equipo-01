2d723f430804718c12b01b6219718335dbba3455
pellomugica@gmail.com <pellomugica@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-05-09 13:14:03 +0000

    Botonerah bien puesto

--- Ventana/src/proyecto/p4/Ventana/Button/BotoneraH.java
+++ Ventana/src/proyecto/p4/Ventana/Button/BotoneraH.java
@@ -52,6 +52,7 @@ public class BotoneraH extends JPanel {
             .addComponent(oldWarriorButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+        
     }
 
 //	@Override
