34fab9af5ea6699ed47cf76bfebfbe5c2d9ab3ff
julentv@gmail.com <julentv@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-05-12 14:42:21 +0000

    reflectividad mejorada

--- Utilities/src/proyect/Reflectividad.java
+++ Utilities/src/proyect/Reflectividad.java
@@ -15,9 +15,7 @@ import java.util.zip.ZipEntry;
 
 
 public class Reflectividad {
-	//fichero que dispone la direccion de los jar
 
-	
 	/** 
 	 * Devuelve un arrayList con una instancia de cada clase instanciable en el jar
 	 * 
@@ -27,8 +25,7 @@ public class Reflectividad {
 	 */
 	public static ArrayList <Object> jarInstance (File file) throws IOException, IllegalArgumentException{
 		ArrayList <Object> objectArray= new ArrayList <Object>();
-		
-		//si el tamaño del nombre del fichero es menor de 5 lanza la excepcion
+		//si el tamaño del nombre el array es menor de 5 lanza la excepcion
 		if (file.getName().length()<5){
 			throw new IllegalArgumentException("The jar file´s name must have more than 4 characters");
 		}
@@ -36,94 +33,79 @@ public class Reflectividad {
 		if (file.getName().substring(file.getName().length()-4).equals(".jar")){
 			JarFile jarFile= new JarFile (file.getAbsolutePath());
 			
+			URL[] jarFileURLs= {file.toURI().toURL()};
+			System.out.println("Loading jar file: " + file);
+			ClassLoader classLoader = new URLClassLoader(jarFileURLs, Reflectividad.class.getClassLoader());
+			
 			//introduce en el enumerador todas los componentes de jar
 			Enumeration<?> entries=jarFile.entries();
-			
 			//recorre todas los componentes
 			while(entries.hasMoreElements()){
 				ZipEntry entry= (ZipEntry) entries.nextElement();
 				String nom=entry.getName();
-				//System.out.println(nom);
-				nom=nom.replaceAll("/", ".");
-				nom=nom.replaceAll(".class", "");
-				//System.out.println(nom);
-				URL[] jarFileURLs= {file.toURI().toURL()};
-				ClassLoader classLoader = new URLClassLoader(jarFileURLs);
-				try {
-					Class <?> clase= Class.forName(nom, false, classLoader);
-					Object instance = clase.newInstance();
-					objectArray.add(instance);
-				} catch (InstantiationException e) {
-				} catch (IllegalAccessException e) {
-				} catch (ClassNotFoundException e) {
-				} catch	(NoClassDefFoundError ncde){
+				if (nom.endsWith(".class"))
+				{
+					nom=nom.replaceAll("/", ".");
+					nom=nom.replaceAll(".class", "");
+					try {
+						System.out.println("Loading class: " + nom);
+						Class <?> clase= Class.forName(nom, false, classLoader);
+						Object instance = clase.newInstance();
+						objectArray.add(instance);
+					} catch (InstantiationException e) {
+						//e.printStackTrace(System.out);
+					} catch (IllegalAccessException e) {
+						//e.printStackTrace(System.out);
+					} catch (ClassNotFoundException e) {
+						//e.printStackTrace(System.out);
+					} catch	(NoClassDefFoundError e){
+						e.printStackTrace(System.out);
+					}
 				}
 			}
-			}
-		else{
+		}
+		else
+		{
 			throw new IllegalArgumentException("The file name in not correct");
 		}
 		return objectArray;
 	}
-	/**
-	 * Carga todos las instancias de los objetos instanciables de los jar
-	 * disponibles en el directorio indicado en la primera linea del fichero txt indicado
-	 * @param direccion Direccion del fichero txt del que se obtendra la direccion del directorio
-	 * en el que se buscaran los jar.
-	 * @return
-	 */
-	public static ArrayList <Object> instanciarDireccion(String direccion){
-		//cargar fichero con la direccion en donde buscar los jar
-		File file= new File (direccion);
+	public static void main(String[] args) throws IllegalArgumentException, IOException {
+		//txt donde se almacena la direccion de los jar
+		File file= new File ("LanguagesFile"); 
 		
-		FileInputStream fileInputStream;
-		ArrayList<Object> instances= new ArrayList<Object>();
-		//recuperar del fichero el directorio de los jar
 		try {
-			fileInputStream = new FileInputStream(file);
+			FileInputStream fileInputStream = new FileInputStream(file);
 			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
 			BufferedReader bufferedReader= new BufferedReader(inputStreamReader);
 			String line = bufferedReader.readLine();
-			//cargar el fichero de los jar
 			File folder= new File (line);
-			//directorios dentro del directorio
+			//guardar en el array los archivos de la carpeta
 			String [] files = folder.list();
 			
 			//reconocimiento y procesamiento de los jar
 			for (String s: files){
+
 				if (s.length()>4){
 					//si es un jar entra en el if
 					if (s.substring(s.length()-4, s.length()).equals(".jar")){
 						ArrayList<Object> idiomas=jarInstance(new File(line+"\\"+s));
+						System.out.println("instancias disponibles:");
+						System.out.println(idiomas.size());
 						for (Object ob: idiomas){
-							instances.add(ob);
-						
-						}
-						
+								System.out.println(ob);	
 						}
 					}
 				}
-// comprobar las instancias realizadas			
-//			for (Object ob: instances){
-//					System.out.println(ob);
-//			}
-			return instances;
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return null;
 		}
-
-	}
-	public static void main(String[] args) throws IllegalArgumentException, IOException {
-		final String direccion="LanguagesFile";
-		instanciarDireccion(direccion);
-	
-	
 	}
 
 }
