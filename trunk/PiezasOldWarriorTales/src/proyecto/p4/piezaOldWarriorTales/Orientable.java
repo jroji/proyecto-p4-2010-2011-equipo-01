6acbb64f283d22be219cc3fbda4ec5ffc19b1a35
julentv@gmail.com <julentv@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-05-12 15:36:20 +0000

    borrado de juego, piezasOldWarriorTales y terrenos

--- trunk/PiezasOldWarriorTales/src/proyecto/p4/piezaOldWarriorTales/Orientable.java
+++
@@ -1,8 +0,0 @@
-package proyecto.p4.piezaOldWarriorTales;
-
-import proyecto.p4.piezaOldWarriorTales.Orientations;
-
-public interface Orientable {
-	public void setOrientation (Orientations orientation);
-
-}
--- trunk/PiezasOldWarriorTales/src/proyecto/p4/piezaOldWarriorTales/Orientations.java
+++
@@ -1,8 +0,0 @@
-package proyecto.p4.piezaOldWarriorTales;
-/* enumerador que recoge las direcciones posibles en las 
- * que puede estar orientada una pieza
- * 
- */
-public enum Orientations {
-	North,South,East,West
-}
