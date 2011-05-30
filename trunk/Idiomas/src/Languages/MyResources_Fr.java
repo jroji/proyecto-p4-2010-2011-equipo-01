f725e61adb03ee304a933f7bb384dff750a02278
raquel.cagigas2 <raquel.cagigas2@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-05-30 14:26:03 +0000

    correcciones de frances

--- trunk/Idiomas/src/Languages/MyResources_Fr.java
+++ trunk/Idiomas/src/Languages/MyResources_Fr.java
@@ -7,10 +7,10 @@ public class MyResources_Fr extends ListResourceBundle{
 	
 	public String [][] contenido={{"label_accept","Accepter"},{"label_exit","Sortir"},{"label_newGame","Nouveau Jeu"},
 			{"label_options","Options"},{"label_quickGame","Rapide Jeu"},{"label_customGame","Custom Jeu"},
-			{"label_newPlayer","Nouveau Joeur"},{"label_tutorial","Tutoriel"},{"label_gold","Or"},{"label_units","Unitï¿½s"},
-			{"label_credits","CrÃ©dits"},{"label_mapEditor","Ã‰diteur de cartes"},{"label_reclute","Recruter"},{"label_langauge","Langue"},
-			{"label_sound","Son"},{"label_attack","Attaque"},{"label_defense","DÃ©fense"},{"label_movement","Mouvement"},
-			{"label_probability","ProbabilitÃ©"},{"label_back","Retour"},{"AtacarYHuir","Attaque et de s'enfuir"},{"AtaqueCertero","Attaque prÃ©cise"},
+			{"label_newPlayer","Nouveau Joeur"},{"label_tutorial","Tutoriel"},{"label_gold","Or"},{"label_units","Unités"},
+			{"label_credits","Crédits"},{"label_mapEditor","Éditeur de cartes"},{"label_reclute","Recruter"},{"label_langauge","Langue"},
+			{"label_sound","Son"},{"label_attack","Attaque"},{"label_defense","Défense"},{"label_movement","Mouvement"},
+			{"label_probability","Probabilité"},{"label_back","Retour"},{"AtacarYHuir","Attaque et de s'enfuir"},{"AtaqueCertero","Attaque précise"},
 			{"AtaqueEnZona","Zone d'attaque"},{"Cegar","Blind"},{"Convertir","Transformer"},{"CurarEstados","Etats Cure"},{"CurarZona","Zone Cure"},
 			{"Envenenar","Poison "},{"LanzarSuriquen","Paru le shuriken"},{"Sprint","Sprint"},{"Yes","Oui"},{"No","No"}
 	
