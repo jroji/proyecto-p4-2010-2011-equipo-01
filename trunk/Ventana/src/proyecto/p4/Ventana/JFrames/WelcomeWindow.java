9e1e2af80874fb3f43680040a2364fdd8e158538
julentv@gmail.com <julentv@gmail.com@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-05-30 14:07:32 +0000

    listener (quitar while(true))

--- trunk/Ventana/src/proyecto/p4/Ventana/JFrames/WelcomeWindow.java
+++ trunk/Ventana/src/proyecto/p4/Ventana/JFrames/WelcomeWindow.java
@@ -1,11 +1,17 @@
 package proyecto.p4.Ventana.JFrames;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ResourceBundle;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 import proyecto.p4.Ventana.Button.BotoneraH;
 import proyecto.p4.Ventana.Button.OldWarriorButton;
@@ -13,25 +19,27 @@ import proyecto.p4.Ventana.JPanels.NickPanel;
 import proyecto.p4.Ventana.JPanels.SelectGamePane;
 import Languages.*;
 
-public class WelcomeWindow extends JFrame{
+public class WelcomeWindow extends JFrame implements ActionListener{
 	
 	NickPanel nicks = new NickPanel();
 	BotoneraH botonera;
 	SelectGamePane game = new SelectGamePane();
 	JLabel fondo = new JLabel(new ImageIcon(getClass().getResource("/img/fondowelcome.jpg")));
+	ResourceBundle language;
+	boolean sound;
 	
 	public WelcomeWindow(ResourceBundle language,boolean Sound){
 
 		botonera= new BotoneraH(language.getString("label_accept"),language.getString("label_exit"),language.getString("label_newPlayer"));
-
-
+		this.setResizable(false);
+		this.language=language;
+		sound=Sound;
 		this.setSize(1024, 728);
 		JLayeredPane layer = new JLayeredPane();
 		this.setLayout(null);
 		this.add(layer);
-		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		layer.setBounds(0, 0, 1024, 728);
-		
 		layer.add(fondo, new Integer (0));
 		layer.add(game, new Integer (1));
 		layer.add(nicks, new Integer (2));
@@ -42,27 +50,53 @@ public class WelcomeWindow extends JFrame{
 		nicks.setBounds(500, 380, 500, 500);
 		botonera.setBounds(50, 550, 600, 200);
 		this.setVisible(true);
-		    while(true){
-		   		if(botonera.getButtonP1()){
-		   			botonera.setButtonP1(false);
-		   			this.dispose();
-					new MainWindow(language,Sound);
-				}else if(botonera.getButtonP2()){
-					botonera.setButtonP2(false);
-					this.dispose();
-		       }
-				else if(botonera.getButtonP3()){
-					botonera.setButtonP3(false);
-					new NewPlayerWindow();
-
-		}
-		}
+		((OldWarriorButton)botonera.getComponent(0)).getOldWarriorButton().addActionListener(this);
+		((OldWarriorButton)botonera.getComponent(1)).getOldWarriorButton().addActionListener(this);
+		((OldWarriorButton)botonera.getComponent(2)).getOldWarriorButton().addActionListener(this);
+		//¿?¿?¿?¿?¿? action listener ¿?¿?¿?¿?¿?
+//		    while(true){
+//		   		if(botonera.getButtonP1()){
+//		   			botonera.setButtonP1(false);
+//		   			this.dispose();
+//					new MainWindow(language,Sound);
+//				}else if(botonera.getButtonP2()){
+//					botonera.setButtonP2(false);
+//					//this.dispose();
+//					System.exit(0);
+//		       }
+//				else if(botonera.getButtonP3()){
+//					botonera.setButtonP3(false);
+//					new NewPlayerWindow();
+//
+//		}
+//		}
+		    
 	}
 	
 	public static void main(String [] args){
-		ResourceBundle language = new MyResources_Es();
-		WelcomeWindow x = new WelcomeWindow(language,true);
-		x.setVisible(true);
+		ResourceBundle language = new MyResources_En();
+		WelcomeWindow x = new WelcomeWindow(language,false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==((OldWarriorButton) botonera.getComponent(0)).getOldWarriorButton())
+		{
+			botonera.setButtonP1(false);
+			new MainWindow(language,sound);
+			this.dispose();
+		}else{
+			if(e.getSource()==((OldWarriorButton) botonera.getComponent(1)).getOldWarriorButton()){
+				botonera.setButtonP2(false);
+				this.dispose();
+				System.exit(0);
+			}else{
+				if(e.getSource()==((OldWarriorButton) botonera.getComponent(2)).getOldWarriorButton()) {
+					botonera.setButtonP3(false);
+					new NewPlayerWindow();
+				}
+			}
+		}
 		
 	}
 }
