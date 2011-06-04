58b6da3f5e8e657e0e6c19720ce4e34a45ddcfa8
raquel.cagigas2 <raquel.cagigas2@da1d19fa-bcca-0322-a297-d80d81363e92>
2011-06-04 09:56:03 +0000

    comentarios

--- JuegoOldWarriorTales/src/proyecto/p4/JuegoOldWarriorTales/JuegoOldWarriorTales.java
+++ JuegoOldWarriorTales/src/proyecto/p4/JuegoOldWarriorTales/JuegoOldWarriorTales.java
@@ -16,17 +16,26 @@ import proyecto.p4.Ventana.JFrames.MainWindow;
 import proyecto.p4.piezaOldWarriorTales.Unidades.Arquero;
 
 
+
 import ConnectionInterface.PieceJDBC;
 import ConnectionInterface.storableInDataBase;
 
 public class JuegoOldWarriorTales extends Juego implements storableInDataBase{
 	
+	/**
+	 * Constructor por defecto
+	 */
 	public JuegoOldWarriorTales (){
 		name="Old Warrior Tales";
 		rutaImagen= "src/img/lgo.jpg";
 	}
 
-	
+	/**
+	 * Método que se encarga de decidir que atributos serán guardados en la base de datos.
+	 * @throws SecurityException indica una violación de la seguridad.
+	 * @throws NoSuchFielException indica que la clase no tiene el atributo con el nombre especificado.
+	 * @return array es un ArrayList que contiene aquellos atributos que se guardarán en la base de datos.
+	 */ 
 
 	@Override
 	public ArrayList<Field> fieldsToStore() throws SecurityException,
@@ -35,12 +44,18 @@ public class JuegoOldWarriorTales extends Juego implements storableInDataBase{
 		array.add(this.getClass().getSuperclass().getDeclaredField("name"));
 		array.add(this.getClass().getSuperclass().getDeclaredField("jugador1"));
 		array.add(this.getClass().getSuperclass().getDeclaredField("jugador2"));
+
 		array.add(this.getClass().getSuperclass().getDeclaredField("playing"));
 	
+
 		return array;
 	}
 	
-	
+	/**
+	 * Método que se encaga de introducir un nuevo juego en la base de datos. Además insertará
+	 * las casillas y las piezas pertenecientes al mismo.
+	 * @return un entero que será el número de juegos introducidos.
+	 */
 	@Override
 	public int insertIntoDataBase(){
 		PieceJDBC p;
@@ -48,6 +63,7 @@ public class JuegoOldWarriorTales extends Juego implements storableInDataBase{
 			p = new PieceJDBC();
 			p.insert(this.getClass().getSimpleName(), this);
 			
+			//se recorre el tablero e inserta las piezas y casillas que están en él.
 			for(int i=0; i<this.tablero.getBoard().length;i++)
 			{
 				for(int j=0; j<this.tablero.getBoard()[i].length;j++)
@@ -67,11 +83,14 @@ public class JuegoOldWarriorTales extends Juego implements storableInDataBase{
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (SQLException e) {
+			//si el juego existe da la opción de sobrescribir
 			e.printStackTrace();
 			Object [] option ={"SI","NO"};
 			int pane=JOptionPane.showOptionDialog(null,
-				    "¿Desea sobreescribir? ","Sobreescribir",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,option,option[0]);
+				    "¿Desea sobrescribir? ","Sobrescribir",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,option,option[0]);
 			
+			//si desea sobrescribir borra el juego con la misma clave primaria que la que se pretende 
+			//insertar e inserta el nuevo juego.
 			if(pane==0){
 				deleteFromDataBase();
 				return insertIntoDataBase();
@@ -87,14 +106,19 @@ public class JuegoOldWarriorTales extends Juego implements storableInDataBase{
 		}
 		return 1;
 		}
-	
+	/**
+	 * Método que se encarga de borrar de la base de datos el juego especificado.
+	 * @return un entero que indica el número de juegos borrados.
+	 */
 	@Override
 	public int deleteFromDataBase() {
 		
+
 			DeleteGame del= new DeleteGame(this);
 			del.start();
 			//tablero es un atributo de juego de la clase mapa/board que es un array de casillas
 		//p.remove(name, (storableInDataBase)this.getTablero());
+
 		
 		
 	
@@ -103,7 +127,10 @@ public class JuegoOldWarriorTales extends Juego implements storableInDataBase{
 
 
 
-
+	/**
+	 * Método que introduce en un ArrayList el contenido de la tabla JuegoOldWarriorTales.
+	 * @return array que contiene la información de dicha tabla.
+	 */
 	@Override
 	public ArrayList<storableInDataBase> takeOutFromDataBase() {
 		
@@ -177,7 +204,6 @@ public class JuegoOldWarriorTales extends Juego implements storableInDataBase{
 
 
 
-	@Override
 	public void throwWindow(ResourceBundle arg0, boolean arg1) {
 		new MainWindow(arg0,arg1,this);	
 	}
